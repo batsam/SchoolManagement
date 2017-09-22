f65b5616fc03dabbeaec09d0f17b29203fc0dc4c
Ex Soisambat <ichsambath@gmail.com>
2017-09-22 14:20:57 +0700

    edit

--- storage/framework/views/61970c356221ddf2ac6e82107ac5b64856f70e9e.php
+++ storage/framework/views/61970c356221ddf2ac6e82107ac5b64856f70e9e.php
@@ -3,12 +3,12 @@
         <?php if(rand(1,100) == 100): ?>
             <i class="voyager-rum-1"></i> Made with rum and even more rum
         <?php else: ?>
-            Made with <i class="voyager-heart"></i> by <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
+            Made with <i class="voyager-heart"></i> by <a href="https://github.com/batsam" target="_blank">Batsam</a>
         <?php endif; ?>
         <?php  $version = Voyager::getVersion();  ?>
-        <?php if(!empty($version)): ?>
+        <!-- <?php if(!empty($version)): ?>
             - <?php echo e($version); ?>
 
-        <?php endif; ?>
+        <?php endif; ?> -->
     </div>
 </footer>
